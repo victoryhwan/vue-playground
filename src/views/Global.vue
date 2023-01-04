138607cc77144037c747c2c584569c13c1596aec
sooyeon.cho <sooyeon.cho@lemonhc.com>
2023-01-04 17:09:50 +0900

    :recycle: <script setup> 문법 수정

--- src/views/Global.vue
+++ src/views/Global.vue
@@ -4,29 +4,21 @@
           전역 컴포넌트 클릭해보세요. {{msg}}아래에 글씨가 나옵니다.
       </button>
       <!--자식 컴포넌트의 버튼을 클릭하면 부모 컴포넌트에 있는 enlarge-text 이벤트를 발생시킬 수 있다-->
-      <button @click="$emit('enlarge-text')">{{msg}}텍스트 확대</button>
+      <button >{{msg}}텍스트 확대</button>
       <br>
       <span class="span">{{ title }}</span>
     </div>
   </template>
    
-  <script>
-  export default {
-    name: 'Global',
-    props: ["msg"],
-    emits: ['enlarge-text','my-log'],
-    data () {
-      return {
-        title: ''
-      }
-    },
-    methods: {
-      showTitle () {
-        this.$emit('my-log')
-        // this.title = 'Global Component TEST !!'
-      }
+  <script setup>
+  import { defineEmits, defineProps } from 'vue';
+
+    const emit = defineEmits(['enlarge-text','my-log'])
+    const props = defineProps({ msg: String })
+    const title = ''
+    const showTitle = () => {
+      emit('my-log')
     }
-  }
   </script>
    
   <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="about">
     <div>{{postFontSize}}</div>
-    <Global v-on:my-log="Alert" :msg="'타이틀'" @enlarge-text="postFontSize += 0.1"></Global>
+    <Global @my-log="Alert" :msg="'타이틀'" @enlarge-text="changeFont"></Global>
     <h1>This is an Home page</h1>
   </div>
 </template>
 
-<script>
-export default {
-  data(){
-    return {
-      postFontSize: 1
-    }
-  },
-  methods:{
-    Alert(){
-      alert("!!")
-    }
-  }
+<script setup>
+import Global from './Global.vue'
+import { ref } from 'vue';
+
+const postFontSize = ref(1)
+const Alert = () => { alert("!!") }
+const changeFont = () => {
+  postFontSize.value += 0.1
 }
+
 </script>
 <style>
 @media (min-width: 1024px) {
