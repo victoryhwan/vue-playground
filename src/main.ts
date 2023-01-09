b679031947c281032af10d870e7895ccfb7995d1
sooyeon.cho <sooyeon.cho@lemonhc.com>
2023-01-09 17:02:47 +0900

    🐛 bootStrap 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
 import globalFunc from './globalFunc'
 import initFunc from './initFunc'
 import BootstrapVue3 from 'bootstrap-vue-3';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
                 .use(initFunc)
