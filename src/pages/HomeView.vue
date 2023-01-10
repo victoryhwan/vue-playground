719f58b46aa35be71e23ca00e4a809b4f214e95a
kihwan.lee <kihwan.lee@lemonhc.com>
2023-01-10 18:14:51 +0900

    modal 컴포넌트 프로토타입 추가

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,15 +1,19 @@
 
 <template>
-  <div class="about">
-    <div>{{postFontSize}}</div>
-    <h1>This is an Home page</h1>
+  <div class="">
+    <h1>Home</h1>
   </div>
   <div>UserId : {{ userStore.user.UserNm }}</div>
+  <!-- 컴포넌트 MyModal -->
+  <ModalComp @close="closeModal()" @ok="okModal()" v-if="modal">
+  </ModalComp>
+  <b-button variant="primary" @click="openModal()" >modal</b-button>
 </template>
 
 <script setup>
 import { ref, provide } from 'vue';
 import { useUserStore } from '@/stores/pinia/user.store';
+import ModalComp from '@/components/ui/ModalComp.vue';
 
 const userStore = useUserStore()
 const location = ref('북극')
@@ -24,10 +28,18 @@ provide('location', {
   updateLocation
 })
 
-const postFontSize = ref(1)
-const Alert = () => { alert("!!") }
-const changeFont = () => {
-  postFontSize.value += 0.1
+
+
+const modal = ref(false)
+const openModal = () => {
+  modal.value = true
+}
+const closeModal = () => {
+  modal.value = false
+}
+const okModal = () => {
+  modal.value = false
+  console.log("OK")
 }
 
 </script>
